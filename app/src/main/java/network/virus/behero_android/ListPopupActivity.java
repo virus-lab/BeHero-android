6a4d6e431c301a09d236d1f6619964ee26ec128e
쥐미니\LG <jmeef0802@gmail.com>
2018-01-30 22:10:39 +0900

    reAlert, callAlert

--- app/src/main/java/network/virus/behero_android/ListPopupActivity.java
+++ app/src/main/java/network/virus/behero_android/ListPopupActivity.java
@@ -1,7 +1,13 @@
 package network.virus.behero_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import network.virus.behero_android.popupWindow.ReAlert;
 
 public class ListPopupActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class ListPopupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_popup);
+
+        Button report_btn = findViewById(R.id.report_btn);
+        report_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View V){
+                Intent intent = new Intent(ListPopupActivity.this,ChangePW_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button reAlert_btn = findViewById(R.id.reAlert_btn);
+        reAlert_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(ListPopupActivity.this,ReAlert.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/network/virus/behero_android/popupWindow/CancelAlert.java
@@ -0,0 +1,36 @@
+package network.virus.behero_android.popupWindow;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+
+import network.virus.behero_android.R;
+
+/**
+ * Created by LG on 2018-01-28.
+ * Login alert Dialog
+ */
+
+public class CancelAlert extends DialogFragment{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.write_cancel_alert)
+                .setPositiveButton(R.string.okButton, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Yes
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
